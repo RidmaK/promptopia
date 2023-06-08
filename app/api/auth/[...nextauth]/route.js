bfb7d437c6f2c3a11d559ba98df52e9b43b11ca5
RidmaAthukorala <ridma@sonicdevs.com>
2023-06-08 17:56:51 +0530

    new

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -8,10 +11,29 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+  },
   async signIn({ profile }) {
     try {
       await connectToDB();
+
+      //   check if user is existing
+      const userExists = await User.findOne({ email: profile.email });
+
+      //   if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
       return true;
     } catch (error) {
       console.log(error);
